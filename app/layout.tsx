6c705bc0d1d430b1c232f6407f8ce2c479483b97
YabloYescobar <joshuanewe@gmail.com>
2024-09-19 03:52:01 -0700

    added dark mode toggle from shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import './globals.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
           <header>
             <SignedOut>
               <SignInButton />
@@ -15,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </SignedIn>
           </header>
           <main>{children}</main>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
 
     <div className="template">
+      <ModeToggle />
       <h1>Template</h1>
       <p>
         This is a template for a Next.js project with TypeScript, ESLint, Prettier, and Tailwind CSS.
